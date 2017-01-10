fd43e8ea69f8484a0a18c46bac3d8dc24f1e759c
Martijn de Valk <info@martijndevalk.nl>
2017-01-10 23:18:02 +0100

    liquid animation

--- src/js/modules/splash-anim.js
+++ src/js/modules/splash-anim.js
@@ -1,10 +1,23 @@
 import TweenMax  from '../../js/vendor/gsap/src/uncompressed/TweenMax';
+import TimelineMax  from '../../js/vendor/gsap/src/uncompressed/TimelineMax';
 import MorphSVGPlugin from '../../js/vendor/gsap/src/uncompressed/plugins/MorphSVGPlugin';
 import DrawSVGPlugin from '../../js/vendor/gsap/src/uncompressed/plugins/DrawSVGPlugin';
 
 export const splashAnim = () => {
 
-    var pathB = $('#motionPathB');
+  TweenMax.set($(".splash-anim .svg"), { visibility: 'visible'});
+
+  const tlBubbleGroup1 = new TimelineMax();
+  const tlBubbleGroup2 = new TimelineMax();
+  const tlBubbleGroup3 = new TimelineMax();
+  const tlBubbleGroup4 = new TimelineMax();
+  const tlBubbleGroup5 = new TimelineMax();
+
+  tlBubbleGroup1.staggerTo('#bubbleGroup1 circle', 3, {attr: {cy: 200}, repeat: -1 }, 0.6);
+  tlBubbleGroup2.staggerTo('#bubbleGroup2 circle', 3, {attr: {cy: 200}, repeat: -1 }, 0.6);
+  tlBubbleGroup3.staggerTo('#bubbleGroup3 circle', 3, {attr: {cy: 200}, repeat: -1 }, 0.6);
+  tlBubbleGroup4.staggerTo('#bubbleGroup4 circle', 3, {attr: {cy: 200}, repeat: -1 }, 0.6);
+  tlBubbleGroup5.staggerTo('#bubbleGroup5 circle', 3, {attr: {cy: 200}, repeat: -1 }, 0.6);
 
     // TweenMax.set("#motionPathB", {opacity: 0.2});
     // TweenMax.set("#motionPathE", {opacity: 0.2});
