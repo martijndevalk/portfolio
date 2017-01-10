cac5e23cf239450c05fb71c0e5de8bc4af422835
NL\martijn.de.valk <martijn.de.valk@valtech.nl>
2017-01-10 21:12:51 +0100

    Splash anim updated

--- src/js/modules/splash-anim.js
+++ src/js/modules/splash-anim.js
@@ -1,11 +1,18 @@
-import TweenMax  from '.././js/vendor/gsap/src/uncompressed/TweenMax';
-import MorphSVGPlugin from '.././js/vendor/gsap/src/uncompressed/plugins/MorphSVGPlugin';
-import DrawSVGPlugin from '.././js/vendor/gsap/src/uncompressed/plugins/DrawSVGPlugin';
+import TweenMax  from '../../js/vendor/gsap/src/uncompressed/TweenMax';
+import MorphSVGPlugin from '../../js/vendor/gsap/src/uncompressed/plugins/MorphSVGPlugin';
+import DrawSVGPlugin from '../../js/vendor/gsap/src/uncompressed/plugins/DrawSVGPlugin';
 
 export const splashAnim = () => {
 
-    var path = MorphSVGPlugin.pathDataToBezier("#motionPath");
+    var pathB = $('#motionPathB');
 
-    TweenMax.to("#circle", 5, {bezier:{values:path, type:"cubic"}, ease:Linear.easeNone, repeat:-1});
+    // TweenMax.set("#motionPathB", {opacity: 0.2});
+    // TweenMax.set("#motionPathE", {opacity: 0.2});
+    //
+    // TweenMax.to("#motionPathB-rect1", 4.2, {bezier: {values: pathB, type: "cubic", autoRotate: true}, ease: Sine. easeOut, repeat: -1});
+    //
+    // TweenMax.to("#motionPathE-circle1", 4.2, {bezier: {values: pathE, type: "cubic", autoRotate: true}, ease: Sine. easeOut, repeat: -1});
+    //
+    // TweenMax.to("#motionPathC-rect", 4.2, {bezier: {values: pathC, type: "cubic", autoRotate: true}, ease: Sine. easeOut, repeat: -1});
 
 };
